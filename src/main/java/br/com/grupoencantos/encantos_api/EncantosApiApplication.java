e53ffcb8f17916854eb807905e3a9c83c5b03340
giuliobronson <giuliobronson@gmail.com>
2025-04-02 19:39:51 -0300

    Commit inicial

---
+++ src/main/java/br/com/grupoencantos/encantos_api/EncantosApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.grupoencantos.encantos_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EncantosApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EncantosApiApplication.class, args);
+	}
+
+}
